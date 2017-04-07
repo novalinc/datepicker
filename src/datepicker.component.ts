6550cdfa76ff195338eeb1ed6695a734ab13d2cd
Hlulani Mhlongo <mhlulani@gmail.com>
2017-04-07 19:32:35 +0200

    Fixed default positioning of picker

--- src/datepicker.component.ts
+++ src/datepicker.component.ts
@@ -20,7 +20,7 @@ import { DatepickerService } from './datepicker.service';
                 <div [ngClass]="{ 'glyphicon': true, 'glyphicon-calendar': !isTime, 'glyphicon-time': isTime, 'center-block': true}"></div>
             </div>
         </div>
-        <div *ngIf="opened" class="nldp-widget dropdown-menu" style="display: block; bottom: auto;">
+        <div *ngIf="opened" class="nldp-widget dropdown-menu" style="top: auto; left: auto; ">
             <div *ngIf="currentView === 'day'" class="datepicker-day">
                 <table class="table-condensed">
                     <thead>
@@ -708,8 +708,8 @@ export class DatepickerComponent implements ControlValueAccessor, OnInit {
     }
 
     onBlur(): void {
-        this.propagateTouch(this.selectedDate);
-        this.opened = false;
+        // this.propagateTouch(this.selectedDate);
+        // this.opened = false;
     }
 
     private _selectedDate: Date;
