16c40434979df4a4af59f748834e8441f65d5b77
Hlulani Mhlongo <mhlulani@gmail.com>
2018-09-19 20:38:56 +0200

    Redone Timepicker make it jumbo size

--- projects/datepicker/src/lib/popup/time/time.component.ts
+++ projects/datepicker/src/lib/popup/time/time.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import moment from 'moment';
 import { DatepickerService } from '../../service/datepicker.service';
+import { IconDefinition, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'dp-time',
@@ -9,38 +10,57 @@ import { DatepickerService } from '../../service/datepicker.service';
 })
 export class TimeComponent implements OnInit {
 
-    hours: number[];
-    minutes: number[];
+    upIcon: IconDefinition = faAngleUp;
+    downIcon: IconDefinition = faAngleDown;
+
+    time: Date;
 
     constructor(private _service: DatepickerService) {
-        this.hours = [];
-        this.minutes = [];
-        for (let h = 0; h < 24; h++) {
-            this.hours.push(h);
-        }
 
-        for (let m = 0; m < 60; m += 5) {
-            this.minutes.push(m);
-        }
     }
 
     ngOnInit() {
-
+        this._service.pick$.subscribe(d => {
+            this.time = d;
+        });
     }
 
-    pickHour(event: any): void {
-        console.debug('picking hour: ', event.target.value);
+    changeHour(up: boolean): void {
+
+        let hour = this._change(this._service.pickedDate.getHours(), 24, up);
+
         let time = moment(this._service.pickedDate)
-            .hour(event.target.value)
+            .hour(hour)
             .toDate();
         this._service.pickDate(time);
     }
 
-    pickMinute(event: any): void {
-        console.debug('picking minute: ', event.target.value);
+    changeMinute(up: boolean): void {
+
+        let minute = this._change(this._service.pickedDate.getMinutes(), 60, up);
+
         let time = moment(this._service.pickedDate)
-            .minute(event.target.value)
+            .minute(minute)
             .toDate();
         this._service.pickDate(time);
+    }
+
+    // TODO: test this method
+    private _change(num: number, max: number, up: boolean): number {
+
+        // Use large increments for bigger ranges
+        let amt = max > 24 ? 5 : 1;
+
+        if (up) {
+            if (amt > 1 && (num % amt) > 0) {
+                return num + (amt - num % amt);
+            }
+            return (num + amt) % max;
+        } else {
+            if (num - amt < 0) {
+                return amt > 1 ? max - amt : max - 1;
+            }
+            return num - amt;
+        }
     }
 }
